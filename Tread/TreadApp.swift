c94e0304a56a8514336755c12d7457922bd87a3c
jordeguevara <jordeguevara@gmail.com>
2025-02-09 15:17:55 -0800

    Initial Commit

---
+++ Tread/TreadApp.swift
@@ -0,0 +1,17 @@
+//
+//  TreadApp.swift
+//  Tread
+//
+//  Created by Jorde Guevara on 2/9/25.
+//
+
+import SwiftUI
+
+@main
+struct TreadApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
